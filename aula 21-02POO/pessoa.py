8b9fd093adaff806af54326e684d126bcfb7118b
gabrielmgouvea <bielgouvea.rj@gmail.com>
2025-02-21 21:31:57 -0300

    Create pessoa.py

---
+++ aula 21-02POO/pessoa.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    def __init__(self, nome, genero, cpf):
+        self.nome = nome
+        self.genero = genero
+        self.cpf = cpf
+
+pessoa1 = Pessoa("Jõao", "M", 123456789)
+
