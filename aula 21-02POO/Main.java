d29ffc9eb051e5b564ed8a164894b853acb6e638
gabrielmgouvea <bielgouvea.rj@gmail.com>
2025-02-21 21:32:46 -0300

    Create Main.java

---
+++ aula 21-02POO/Main.java
@@ -0,0 +1,14 @@
+package java;
+
+class Pessoa (
+
+    String nome;
+    String genero;
+    String cpf;
+
+    Pessoa(String nome, String genero, String cpf) {
+        this.nome = nome;
+        this.genero = genero;
+        this.cpf = cpf;
+    }
+)
